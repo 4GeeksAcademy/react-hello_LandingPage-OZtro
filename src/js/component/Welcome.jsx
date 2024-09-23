940a19e0e1bd7c72bbd74ba56ef58ba035c0b644
OZ-tro <odzm302@gmail.com>
2024-09-23 21:25:10 +0000

    Cambio en el boton del welcome

--- src/js/component/Welcome.jsx
+++ src/js/component/Welcome.jsx
@@ -9,7 +9,7 @@ const Welcome = () => {
           <div className="col-12 text-start bg-light p-5 rounded-3 mh-auto"style={{ height: '300px'}}>
             <h1 className=""style={{ fontSize: '80px'}}>A Warm Welcome!</h1>
             <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sed officia nostrum enim aorem ipsum, dolor sit amet consectetur adipisicing elit. Sed officia nostrum enim orem ipsum, dolor sit amet consectetur adipisicing elit. Sed officia nostrum enim orem ipsum, dolor sit amet consectetur adipisicing elit. Sed officia nostrum enim </p>
-            <button type="button" className="btn btn-primary">Primary</button>
+            <button type="button" className="btn btn-primary">Call to action!</button>
           </div>
 
 
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -13,9 +13,6 @@ const Home = () => {
 			<Welcome/>
 			<Cards/>
 			<Footer/>
-
-			
-
 		</div>
 	);
 };
